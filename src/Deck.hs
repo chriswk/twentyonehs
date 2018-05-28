3a45d5917fc131380e5af4ac9f3bdb177968fe0d
Christopher Kolstad <christopher.kolstad@finn.no>
2018-05-28 11:04:16 +0200

    Assert back and forth conversion for deck as well

--- src/Deck.hs
+++ src/Deck.hs
@@ -1,8 +1,19 @@
 module Deck where
 
 import           Card
+import           Data.List  (intercalate)
+import           Text.Regex
 
 type Deck = [Card]
 
 deck :: Deck
 deck = [Card suit rank | suit <- [Clubs .. Spades], rank <- [Two .. Ace]]
+
+split :: Regex
+split = mkRegex ",\\s?"
+
+parseDeck :: String -> Deck
+parseDeck x = map read $ splitRegex split x
+
+prettyPrintDeck :: Deck -> String
+prettyPrintDeck d = intercalate ", " $ map show d
--- test/DeckSpec.hs
+++ test/DeckSpec.hs
@@ -3,9 +3,24 @@ module DeckSpec where
 import           Card
 import           Deck
 import           Test.Hspec
+import           Test.QuickCheck
 
-spec = describe "Deck" $ do
-  it "consists of 52 cards" $ do
-    length deck `shouldBe` 52
-  it "can parse a string into multiple cards" $ do
-    parseDeck "HA, HK, SK, SQ" shouldBe [Card Hearts Ace, Card Hearts King, Card Spades King, Card Spades Queen]
+instance Arbitrary Suit where
+  arbitrary = arbitraryBoundedEnum
+
+instance Arbitrary Rank where
+  arbitrary = arbitraryBoundedEnum
+
+instance Arbitrary Card where
+  arbitrary = Card <$> arbitrary <*> arbitrary
+
+spec =
+  describe "Deck" $ do
+    it "consists of 52 cards" $ do length deck `shouldBe` 52
+    it "can parse a string into multiple cards" $ do
+      parseDeck "HA, HK, SK, SQ" `shouldBe`
+        [Card Hearts Ace, Card Hearts King, Card Spades King, Card Spades Queen]
+    it "handles deck strings with and without space in seperator" $ do
+      parseDeck "HA, HK, SK, SQ" `shouldBe` parseDeck "HA,HK,SK,SQ"
+    it "can parse output of all decks back to deck" $
+      property $ \d -> (parseDeck . prettyPrintDeck) d == (d :: Deck)
